8ddb65c47d122fe5858eea0ae21b495726cdd173
emht <emht@users.noreply.github.com>
2018-03-12 13:11:31 +0530

    Completing MINEAT from MArch challenge.

--- CPP/MINEAT.cpp
+++ CPP/MINEAT.cpp
@@ -2,82 +2,115 @@
  * Minion Chef and bananas
  * Platform: codechef
  * Link: https://www.codechef.com/MARCH18B/problems/MINEAT
- * Concept: You understood the question correctly, read the question more carefully 
- * Status: Failed, TLE error, Iterative approach
+ * Concept: Correct from my side. Missing some critical test cases, IDK which ones :/
+ * Status: Failed
  */
 
 #include <iostream>
 #include <algorithm> //Sort function
-#include <cstring> //memcpy function
 using namespace std;
 
 #define FOR(I, A, B) for(int I = (A); I < (B); ++I)
 
-int check_K(long piles[], int count, int k, long sum, int hour) {
-	long temp_piles[count];
-	memcpy (temp_piles, piles, count * sizeof(long));
-	int j = 0;
-	while (j < count) {
-		if (hour != 0) {
-			if (temp_piles[j] <= k) {
-				hour--;
-				sum = sum - temp_piles[j];
-				j++;
-			}
-			else {
-				temp_piles[j] = temp_piles[j] - k;
-				sum = sum - k;
-				hour--;
-			}
-		}
-		else 
-			break;
-	}
-
-	if (sum > 0) { return 1; }
-	else if (sum == 0 && hour == 0) { return 0; }
-	else if (sum == 0 && hour > 0) { return -1; }
-	else if (sum < 0) { return -1; } 
-}
-
 int main(void) {
 	int testcase;
-	int output, num_pile, hour, k;
+	long int num_pile, hour, k, temp_hour;
+	float temp;
 	cin >> testcase;
+
+	bool decrease = false, increase = false;
 
 	while(testcase--) {
 		cin >> num_pile >> hour;
-		long int pile_size[num_pile], average = 0, total_sum = 0;
+		long int pile_size[num_pile], average = 0, total_sum = 0, temp_sum;
 
 		for (int i = 0; i < num_pile; i++) {	
 			cin >> pile_size[i];
 			total_sum = pile_size[i] + total_sum;
 		}
 
-		// Logic to calculate k
 		average = total_sum / num_pile;
-
-		// Start k with the average size of banana pile, if time left, reduce k else increase.
-		k = average;
-		
+	
 		// Sort the array
 		sort(pile_size, pile_size + num_pile);
-	
-		output = check_K(pile_size, num_pile, k, total_sum, hour);
 
-		while (output != 0) {
-			if (output == 1) {
-				k++;
-			}
-			else if (output == -1) {
-				k--;
-			}		
-			
-			// Call function check_K(k);
-			output = check_K(pile_size, num_pile, k, total_sum, hour);
-		}
+		if (hour == num_pile) {
+			// Initialising k with the largest size
+			k = pile_size[num_pile - 1];
 			cout << k << endl;
-	}
+		}
+		else {
+			// Logic when hour != num_pile
+			k = average;
+			//cout << "K is: " << k << endl;
+			decrease = increase = false;
 
+			temp_sum = total_sum;
+			temp_hour = hour;
+			temp = 0;
+
+			FOR(i, 0, num_pile) {
+				if (pile_size[i] <= k) {
+					temp_sum = temp_sum - pile_size[i];
+					temp_hour--;
+				}
+				else {
+					temp_sum = temp_sum - k;
+					temp_hour--;
+				}
+			}
+
+			temp = float(temp_sum) / temp_hour;
+			//cout << "temp is: " << temp << endl;
+			if (temp < k) { decrease = true;}
+			else if (temp > k) { increase = true;}
+			else if (temp == k) { cout << k << endl; }
+
+			if (increase) {
+				while(temp > k) {
+					k++;
+					//continue with k, final k is the answer
+					temp_sum = total_sum;
+					temp_hour = hour;
+
+					FOR(i, 0, num_pile) {
+						if (pile_size[i] <= k) {
+							temp_sum = temp_sum - pile_size[i];
+							temp_hour--;
+						}
+						else {
+							temp_sum = temp_sum - k;
+							temp_hour--;
+						}
+					}
+					temp = float(temp_sum) / temp_hour;
+				}
+				cout << k << endl;
+			}
+			else if (decrease) {
+				while (temp < k) {
+					// continue with k - 1, k+1 is the answer
+					k--;
+					//cout << "K is: " << k << endl;
+					temp_sum = total_sum;
+					temp_hour = hour;
+
+					FOR(i, 0, num_pile) {
+						if (pile_size[i] <= k) {
+							temp_sum = temp_sum - pile_size[i];
+							temp_hour--;
+						}
+						else {
+							temp_sum = temp_sum - k;
+							temp_hour--;
+						}
+					}
+					temp = float(temp_sum) / temp_hour;
+					//cout << "temp is: " << temp << endl;
+				}
+				cout << k + 1 << endl;
+			}
+		}
+	}
 	return 0;
 }
